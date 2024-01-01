91dd662f78db52be10a5e95e1e268bc45f6ccfa1
SathishMadesh <s747272946@gmail.com>
2024-01-01 14:50:09 +0530

    update

--- student/urls.py
+++ student/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import index, view_student
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>', views.view_student, name='view_student')
+    path('', index),
+    path('<int:id>', view_student, name='view_student')
 ]
